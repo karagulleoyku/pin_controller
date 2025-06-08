fa3e1b694f12eb4f3397c4d197386ed92d70edef
Oyku Karagulle <150177584+karagulleoyku@users.noreply.github.com>
2025-06-08 21:48:53 +0300

    Update main.java

--- main.java
+++ main.java
@@ -4,20 +4,20 @@ public class main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Kullanıcıdan PIN 
+        // Kullanıcıdan PIN almak
         System.out.print("Lütfen 4 haneli PIN kodunuzu girin: ");
-        String girilenPin = scanner.nextLine();
+        String girilenPin = scanner.nextLine(); 
 
-        // Kontrol
+        // PIN 4 haneli mi?
         if (girilenPin.length() != 4 || !girilenPin.matches("\\d{4}")) {
-            System.out.println("PIN kodu 4 basamaklı olmalıdır.");
+            System.out.println("PIN kodu 4 basamaklı olmalıdır."); 
             return;
         }
 
         // Sabit PIN
         String sabitPin = "1234";
 
-        // Bant
+        // Bant gösterimi
         String input = girilenPin + "#" + sabitPin + "#";
         char[] tape = input.toCharArray();
 
